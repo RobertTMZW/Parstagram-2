251d870d4244426522cf3d7b98c2b933230542c9
RobertTMZW <mukuchurart@gmail.com>
2020-12-04 22:48:41 -0500

    First commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application
+{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("1h4cLzIfFcx15rwwsKDJYyylxgfGq07H6dZcaFlf")
+                .clientKey("SBTFB40CFAC5nHcLfFKZjdOknSjHoeVKZRSshcZV")
+                .server("https://parseapi.back4app.com").build()
+        );
+    }
+}
+
